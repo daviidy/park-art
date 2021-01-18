08a5539b242dfb9ecf343ef75f9b21e0de8fdf05
aouman <k2.aouman@gmail.com>
2021-01-18 09:57:22 +0000

    mise en place du tableau de bord freelancer dans le document view-freelencer-home. j'ai créée des includes pour mettre diférrents progression des projets.

---
+++ resources/views/includes/projetRemporter.blade.php
@@ -0,0 +1,35 @@
+<div class="container">
+
+  <div class="row">
+    <div class="col-md-12">
+      <div class="card border-0 mb-3">
+        <div class="row rounded-lg box-shadow no-gutters p-3 shadow align-items-center">
+          <div class="col-md-3 pr-4">
+            <div class="p-2 p-md-0 d-flex justify-content-around align-items-center">
+              <img src="https://oschoolelearning.com/images/courses/logos/1580161319.png" alt="" class="img-fluid " style="width: 50px">
+              <p class="card-text text-dark">Projet2 <span class="pl-1"> <a href="#"><i class="bi bi-pencil-square btn-delete"></i></a></span> </p>
+            </div>
+          </div>
+          <div class="col-md-6">
+            <div class="p-2 p-md-0">
+              <p class="card-text text-dark text-truncate">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo conse</p>
+            </div>
+          </div>
+          <div class="col-md-2">
+            <div class="p-2 p-md-0">
+              <p class="card-text text-dark">5000€</p>
+            </div>
+          </div>
+          <div class="col-md-1">
+            <div class="p-2 p-md-0 justify-content-md-around d-flex align-items-center">
+              <p class="card-text m-0 pr-3 pr-md-0"><a href="#"><i class="bi bi-trash text-danger btn-delete"></i></a></p>
+              <p class="card-text"><a href="#"  target="_blank"><i class="bi bi-eye btn-delete"></i></a></p>
+            </div>
+          </div>
+        </div>
+      </div>
+
+
+    </div>
+  </div>
+</div>
--- resources/views/users/freelancer/home.blade.php
+++ resources/views/users/freelancer/home.blade.php
@@ -1,26 +1,28 @@
 @extends('layouts.menuHomes')
 @section('content')
-  <main class="container-fluid">
-    <form action="{{ route('logout') }}" method="post">
-      @csrf
-      <button type="submit">
-        Deconnecter
-      </button>
-    </form>
-
-    <div class="row">
-      <!--menu left-->
-      <div class="col-3 bg-light d-none side-menu d-md-block">
-      </div>
-      <!--end menu left-->
-      <!--main content-->
-      <div class="col-md-9 main-content">
-        <div class="p-4 shadow mt-4">
-          <h3>Tableau de bord freelance</h3>
+<div class="col-12 col-md-11">
+  <div class="p-md-4">
+    <div class="align-items-center d-flex justify-content-between mb-5 pt-5">
+      <h2 class="h3">Mes projets</h2>
+      <a href="#" class="btn-bg-plus p-2 p-md-3 rounded-lg text-decoration-none text-white shadow">Publier un projet</a>
+    </div>
+    <div class="pt-md-5">
+      <div class="">
+        <ul class="nav nav-pills mb-3" id="pills-tab" role="tablist">
+          <li class="nav-item" role="presentation">
+            <a class="nav-link active" id="pills-home-tab" data-toggle="pill" href="#pills-home" role="tab" aria-controls="pills-home" aria-selected="true">Projet en cours</a>
+          </li>
+          <li class="nav-item" role="presentation">
+            <a class="nav-link" id="pills-profile-tab" data-toggle="pill" href="#pills-profile" role="tab" aria-controls="pills-profile" aria-selected="false">Projets remportés</a>
+          </li>
+        </ul>
+        <div class="tab-content" id="pills-tabContent">
+          <div class="tab-pane fade show active" id="pills-home" role="tabpanel" aria-labelledby="pills-home-tab">@include('includes.projetEnCours')</div>
+          <div class="tab-pane fade" id="pills-profile" role="tabpanel" aria-labelledby="pills-profile-tab">@include('includes.projetRemporter')</div>
         </div>
       </div>
-      <!--end main content-->
-      <div id="overlay"></div>
+
     </div>
-  </main>
+  </div>
+</div>
 @endsection
