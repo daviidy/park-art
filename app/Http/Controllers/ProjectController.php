095f944070934cdb51fc5a7bb2a9249960170c10
Samuel Bouyé <Samuel Bouye>
2021-01-21 08:55:26 +0000

    edit user client profile and change some route redirect register model to register page

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -15,7 +15,7 @@ class ProjectController extends Controller
      */
     public function index()
     {
-        $projects = Project::latest()->paginate(5);
+        $projects = Project::latest();
 
         return view('users.projets.index', compact('projects'));
 
@@ -47,7 +47,7 @@ class ProjectController extends Controller
 
         Project::create($request->all());
 
-        return redirect()->route('client.my-profile.index')
+        return redirect()->route('displayAllMyProjects')
                         ->with('success','Projet Ajouté avec succès.');
     }
 
@@ -90,7 +90,7 @@ class ProjectController extends Controller
 
         $project->update($request->all());
 
-            return redirect()->route('client.my-profile.index')
+            return redirect()->route('displayAllMyProjects')
                             ->with('success','Projet modifié avec succès');
 
     }
@@ -105,17 +105,25 @@ class ProjectController extends Controller
     {
         $project->delete();
 
-        return redirect()->route('client.my-profile.index')
+        return redirect()->route('displayAllMyProjects')
                         ->with('success','Project supprimer avec succès');
     }
 
-
+    /**
+     * Display all projects on nos-projects page
+     * @return Response
+     */
     public function allProjects()
     {
         $projects = Project::all();
         return view('projects.index', compact('projects'));
     }
 
+    /**
+     * Display a project on all projects list 
+     * @param  $project_id
+     * @return Response
+     */
     public function displayProject($project_id)
     {
         $project = Project::find($project_id);
