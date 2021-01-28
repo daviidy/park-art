6adbf763162235719e4f61ba1368f4a9ddf2a981
aouman <k2.aouman@gmail.com>
2021-01-28 18:23:41 +0000

    Suppression de la route test qui a été mise pour la page

--- routes/web.php
+++ routes/web.php
@@ -22,10 +22,6 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/edit', function () {
-    return view('users.freelancer.edit');
-});
-
 //prestataire routes
 Route::get('/nos-prestataires',[FreelanceController::class, 'allFreelancers']);
 Route::get('/prestataire/{id}',[FreelanceController::class, 'freelancerInfo']);
