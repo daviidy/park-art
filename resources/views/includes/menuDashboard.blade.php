a5be5777698a551fa9f1c683a2ab812599c78bf9
aouman <k2.aouman@gmail.com>
2021-01-29 12:50:49 +0000

    Mise à jour de la home page client. cette home page est contituée de trois block qui redirige l'utilisateur sur différent page de son tableau de bords. le chemin est views/users/client/home. La page précédente qui présente ses projets a été déplacée dans un autre ficher qui se trouve dans : views/users/client/projets/index. et en fin mise en place du fonctionnement des boutons du menu.

--- resources/views/includes/menuDashboard.blade.php
+++ resources/views/includes/menuDashboard.blade.php
@@ -2,12 +2,12 @@
   <i class="bi bi-x-circle close-btn position-absolute text-white d-md-none d-block" style="right: 15px"></i>
   <div class="bg-menu-left p-3 p-md-0 rounded-lg shadow">
     <div class="">
-      <div class="text-center p-md-4">
+      <div class="text-center py-md-4">
         @if(Auth::user()->profile_image != "image_default.png")
           <img src="/images/{{ Auth::user()->profile_image }}" alt="avatar" width="50" class="img-fluid rounded-circle">
         @else
-          <img src="/default_image/{{ Auth::user()->profile_image }}" class="card-img-top img-fluid rounded-circle w-50 mx-auto" alt="...">
-        @endif
+          <i class="bi bi-person-circle text-white" style="font-size: 30px;"></i>
+       @endif
 
       </div>
       <div class="">
@@ -16,15 +16,19 @@
               <i class="bi bi-speedometer2"></i><br>
               <span class="font-text-menu">Accueil</span>
             </a>
-            <a href="{{ route('displayAllMyProjects') }}" class="mr-2 py-2 active text-white text-decoration-none">
-              <i class="bi bi-layout-text-window-reverse"></i><br>
-              <span class="font-text-menu">Mes projets</span>
+            <a href="/" class="mr-2 py-2 text-white text-decoration-none d-md-block d-flex align-items-center justify-content-around">
+              <i class="bi bi-speedometer2"></i><br>
+              <span class="font-text-menu">Actions</span>
+            </a>
+            <a href="{{ route('displayAllMyProjects') }}" class="mr-2 py-2 text-white text-decoration-none d-md-block d-flex align-items-center justify-content-around">
+                <i class="bi bi-layout-text-window-reverse"></i><br>
+                <span class="font-text-menu">Mes projets</span>
             </a>
             <a href="{{ route('projects.create') }}" class="mr-2 py-2 text-decoration-none d-md-block d-flex align-items-center justify-content-around">
               <i class="fas fa-paper-plane text-white"></i><br>
               <span class="font-text-menu">Publier</span>
             </a>
-            <a href="{{ url('/nos-prestataires') }}" class="mr-2 py-2 text-decoration-none">
+            <a href="{{ url('/nos-prestataires') }}" class="mr-2 py-2 text-decoration-none d-md-block d-flex align-items-center justify-content-around">
               <i class="bi bi-people-fill text-white"></i><br>
               <span class="font-text-menu">Prestataires</span>
             </a>
@@ -36,7 +40,7 @@
               <i class="bi bi-person-square text-white"></i><br>
               <span class="font-text-menu">Mon profil</span>
             </a>
-            <a href="{{ route('client.my-profile.edit', Auth::user()->id) }}" class="mr-2 py-2 text-decoration-none">
+            <a href="{{ route('client.my-profile.edit', Auth::user()->id) }}" class="mr-2 py-2 text-decoration-none d-md-block d-flex align-items-center justify-content-around">
               <i class="bi bi-gear-fill text-white"></i><br>
               <span class="font-text-menu">Paramètre</span>
             </a>
