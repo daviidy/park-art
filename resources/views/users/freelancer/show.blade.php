6432f5dcd231492b5e3aecaa8b81a6948414e6a8
aouman <k2.aouman@gmail.com>
2021-02-01 15:16:05 +0000

    Mise en place du ficher index

---
+++ resources/views/users/freelancer/show.blade.php
@@ -0,0 +1,62 @@
+@extends('layouts.menu')
+@section('content')
+      <section class="hero-pro-show"></section>
+      <section class="mb-5">
+        <div class="">
+          <div class="position-absolute p-3" style="top:250px; left:50px">
+          @if($project->user->profile_image != "image_default")
+            <img width="150" height="150" src="/images/{{ $project->user->profile_image }}" alt="" class=" rounded-lg">
+          @else
+          <i class="bi bi-person-circle text-secondary rounded-lg" style="font-size: 40px;"></i>
+          @endif
+            <h4 class="pt-2">{{ $project->user->first_name }} {{ $project->user->last_name }}</h4>
+          </div>
+        </div>
+      </section>
+      <section class="p-md-5 p-3">
+        <div class="container-fluid">
+          <div class="row row-cols-1 row-cols-md-2">
+            <div class="col-md-12">
+              <div class="card rounded-lg shadow">
+                <div class="card-body">
+                  <div class="container">
+                    <div class="pt-3">
+                      <div class="d-md-flex justify-content-between align-items-center">
+                        <div class="">
+                          <p class="h6 badge font-weight-light">Catégories :<span class="font-weight-bold ">Web</span> </p>
+                        </div>
+                        <div class="d-md-flex justify-content-between align-items-center">
+                          <p class="h6 badge font-weight-light"> Poster le <span class="font-weight-bold">15/02/2021</span></p>
+                          <p class="h6 badge font-weight-light pl-md-4"><span class="font-weight-bold">{{  count($proposal) }}</span> postulant(s)</p>
+                        </div>
+                      </div>
+                    </div>
+                    <div class="row border rounded-lg mt-3">
+                      <div class="col-md-8 py-2 d-flex justify-content-between align-items-center">
+                        <h4 class="h6">Budget :</h4>
+                        <p class="mb-0">{{ $project->budget }}€</p>
+                      </div>
+                      @if($proposal->find(Auth::user()->id))
+                        <div class="col-md-4 border-left p-3 text-center">
+                            <p>Vous avez deja postulier merci de bien vouloir patienter 😊</p>
+                        </div>
+                      @else
+                        <div class="col-md-4 border-left p-3 text-center">
+                                <span class="btn btn-primary" data-toggle="modal" data-target="#offertModal">Proposer une offre</span>
+                        </div>
+                    </div>
+                    @endif
+                    <div class="row">
+                      <div class="col-md-12 pt-md-4 pt-3">
+                        <h4 class="pb-3 h6">Description</h4>
+                        <p>{{ $project->description }}</p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+@endsection
