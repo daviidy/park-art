9ba418fba55881731bde2ce510e000313fcab544
Kouassi Aouman <k2.aouman@gmail.com>
2021-02-09 12:43:35 +0000

    Modification de la page mes projets dans le tableau de bord client et freelancer. j'ai fait l'alignement des images dd'utilisateur dans cette page.

--- resources/views/includes/projetRemporter.blade.php
+++ resources/views/includes/projetRemporter.blade.php
@@ -4,8 +4,8 @@
     <div class="col-md-12">
       <div class="card border-0 mb-3">
         <div class="row rounded-lg box-shadow no-gutters p-3 shadow align-items-center">
-          <div class="col-md-3 pr-4">
-            <div class="p-2 p-md-0 d-flex justify-content-around align-items-center">
+          <div class="col-md-4 pr-4">
+            <div class="p-2 p-md-0 d-flex justify-content-between align-items-center">
               @if(Auth::user()->profile_image != "image_default.png")
                 <img src="/images/{{Auth::user()->profile_image }}" class="img-fluid" alt="..." style="width: 50px">
                 @else
@@ -16,17 +16,16 @@
           </div>
           <div class="col-md-6">
             <div class="p-2 p-md-0">
-              <p class="card-text text-dark text-truncate">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo conse</p>
+              <p class="card-text text-dark text-truncate p-4">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo conse</p>
             </div>
           </div>
           <div class="col-md-2">
             <div class="p-2 p-md-0">
-              <p class="card-text text-dark">5000€</p>
-            </div>
-          </div>
-          <div class="col-md-1">
-            <div class="p-2 p-md-0 justify-content-md-around d-flex align-items-center">
-              <p class="card-text"><a href="#"  target="_blank"><i class="bi bi-eye btn-delete"></i></a></p>
+              <p class="card-text text-dark d-flex justify-content-between align-items-center">5000€
+                <span class="card-text">
+                  <a href="#"  target="_blank"><i class="bi bi-eye btn-delete"></i></a>
+                </span>
+              </p>
             </div>
           </div>
         </div>
